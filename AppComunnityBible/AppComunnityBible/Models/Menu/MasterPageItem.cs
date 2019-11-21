93fc169ac05f014da35020f8b7c482358603b9fc
Cristian F. Shute <cristian.flores@clariti.net>
2019-11-21 00:16:46 -0500

    -Added plugin for better control of bindings in MVVM pattern. -Added binding with the list of the MasterDetailPage to comply with the MVVM pattern.

--- AppComunnityBible/AppComunnityBible/Models/Menu/MasterPageItem.cs
+++ AppComunnityBible/AppComunnityBible/Models/Menu/MasterPageItem.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿using MvvmHelpers;
+using System;
 
 namespace AppComunnityBible.Models.Menu
 {
-
-    public class MasterPageItem
+    public class MasterPageItem : ObservableObject
     {
+        private string _title;
+        private string _iconSource;
+        private Type _targetType;
+
         public MasterPageItem()
         {
             TargetType = typeof(MasterPageItem);
+            IconSource = string.Empty;
+            Title = string.Empty;
         }
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string IconSource { get; set; }
-        public Type TargetType { get; set; }
+
+        public string Title {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public string IconSource {
+            get => _iconSource;
+            set => SetProperty(ref _iconSource, value);
+        }
+
+        public Type TargetType {
+            get => _targetType;
+            set => SetProperty(ref _targetType, value);
+        }
     }
 }
--- AppComunnityBible/AppComunnityBible/ViewModels/Menu/MainMasterDetailPageViewModel.cs
+++ AppComunnityBible/AppComunnityBible/ViewModels/Menu/MainMasterDetailPageViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using AppComunnityBible.Models.Menu;
+using AppComunnityBible.Views;
 using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -9,9 +12,16 @@ namespace AppComunnityBible.ViewModels.Menu
     {
         public ICommand ItemSelectedCommand { get; private set; }
 
+        public ObservableCollection<MasterPageItem> ListItemSource { get; set; }
+
         public MainMasterDetailPageViewModel()
         {
             ItemSelectedCommand = new Command<SelectedItemChangedEventArgs>(OnItemSelected);
+
+            // Create the pages
+            ListItemSource = new ObservableCollection<MasterPageItem>();
+            ListItemSource.Add(new MasterPageItem() { Title = "Search", TargetType = typeof(PageSearch) });
+            ListItemSource.Add(new MasterPageItem() { Title = "Configuration", TargetType = typeof(PageConfiguration) });
         }
 
         void OnItemSelected(SelectedItemChangedEventArgs e)
